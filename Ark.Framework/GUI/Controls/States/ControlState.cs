857a8fc4fdb5019e9f31a972dcac3ca36c0ef7cf
Rusty Shakleford <gene.cupstid@gmail.com>
2018-11-20 18:05:38 -0600

    dev back to previous state

---
+++ Ark.Framework/GUI/Controls/States/ControlState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Framework.GUI.Controls.States
+{
+    public abstract class ControlState<T> where T : Control
+    {
+        #region [ Members ]
+        protected T _owner;
+        #endregion
+
+        #region [ Constructor ]
+        protected ControlState(T owner) { _owner = owner; }
+        #endregion
+
+        #region [ Rule ]
+        public abstract bool Applies();
+        #endregion
+    }
+
+
+    public class DefaultState : ControlState<Control>
+    {
+        public DefaultState(Control owner) : base(owner) { }
+
+        public override bool Applies()
+        {
+            if (_owner.Hovered == false && _owner.Pressed == false)
+                return true;
+            return false;
+        }
+    }
+
+    public class CheckedState : ControlState<Checkbox>
+    {
+        public CheckedState(Checkbox owner) : base(owner) { }
+
+        public override bool Applies()
+        {
+            if ( _owner.IsChecked == true && 
+                _owner.Hovered == false && 
+                _owner.Pressed == false)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
